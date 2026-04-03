3a42d49a67ca64a5cd11c180b12a284a823562c3
kara7z <kara.ossama12@gmail.com>
2026-04-03 09:58:25 +0100

    updating factory to be compatible with migration and models

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Enums\Gender;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
@@ -27,6 +28,8 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'gender' => fake()->randomElement(Gender::values()),
+            'birthday' => fake()->date(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
